bb0fd23b7173a486bcbb3e502ae8df42a8fa6931
ethandamian <ethandamian@ciencias.unam.mx>
2024-04-16 13:14:58 -0600

    Cambie ruta del app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from controllers.controllerPreferencias import preferencias_blueprint
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("preferenciasFormulario.html")
 
